2bc655f775b380ffdbadeee7a3230f01116ac4b4
cheme <emericchevalier.pro@gmail.com>
2024-05-02 11:33:46 +0200

    adapt db params

--- substrate/client/db/src/parity_db.rs
+++ substrate/client/db/src/parity_db.rs
@@ -58,10 +58,10 @@ pub fn open<H: Clone + AsRef<[u8]>>(
 	}
 
 	let state_col = &mut config.columns[columns::STATE as usize];
-	state_col.preimage = true;
-	state_col.uniform = true;
+	state_col.preimage = !multitree;
+	state_col.uniform = !multitree;
 	state_col.append_only = archive & multi_tree;
-	state_col.ref_counted = true & !state_col.append_only;
+	state_col.ref_counted = !multitree & !state_col.append_only;
 
 	if multi_tree {
 		state_col.multitree = true;
